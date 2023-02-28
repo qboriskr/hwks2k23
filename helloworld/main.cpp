7b83821af80b3d69479c886723357d8c4121124d
boris <boriskr2008@yandex.ru>
2023-02-28 18:45:42 +0400

    fix output string 2

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -3,6 +3,6 @@
 
 int main(int, char **) {
 	std::cout << "build " << version() << std::endl;
-	std::cout << "Hello, world!" << std::endl;
+	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
